e5d073589fa9c1bde73332702fc5fb42cf9cf940
Mohit Mutha <mohit.mutha.ext@siemens.com>
2022-02-15 22:19:51 +0530

    Add cloudfront distribution

--- tf/cloudfront.tf
+++ tf/cloudfront.tf
@@ -6,4 +6,57 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
   comment = "cloudfront-ecs-demo-webapp-origin"
 }
 
+resource "aws_cloudfront_distribution" "cf_distribution" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "cloudfront-ecs-demo-webapp"
+  default_root_object = "index.html"
+  origin {
+    domain_name = aws_s3_bucket.cf-s3-ecs-demo-bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE", "IN", "IR"]
+    }
+  }
+
+  tags = {
+    Environment = "development"
+    Name        = "my-tag"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
 
