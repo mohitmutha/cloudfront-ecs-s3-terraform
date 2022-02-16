50b1ee9e7c6f970282238627a28d5c733b65563c
Mohit Mutha <mohit.mutha.ext@siemens.com>
2022-02-16 09:37:18 +0530

    Final Cloudfront-S3

--- tf/cloudfront.tf
+++ tf/cloudfront.tf
@@ -60,3 +60,32 @@ resource "aws_cloudfront_distribution" "cf_distribution" {
   }
 }
 
+output "cloudfront_dns" {
+  value = aws_cloudfront_distribution.cf_distribution.domain_name
+}
+
+data "aws_iam_policy_document" "cf_s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.cf-s3-ecs-demo-bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "cf_s3_bucket_policy" {
+  bucket = aws_s3_bucket.cf-s3-ecs-demo-bucket.id
+  policy = data.aws_iam_policy_document.cf_s3_policy.json
+}
+
+resource "aws_s3_bucket_public_access_block" "cf_s3_bucket_acl" {
+  bucket = aws_s3_bucket.cf-s3-ecs-demo-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  //ignore_public_acls      = true
+  //restrict_public_buckets = true
+}
--- tf/website.tf
+++ tf/website.tf
@@ -12,10 +12,15 @@ resource "aws_s3_bucket_acl" "cf-s3-ecs-demo-bucket" {
   acl    = "private"
 }
 
+locals {
+  mime_types = jsondecode(file("${path.module}/mime.json"))
+}
+
 resource "aws_s3_object" "website-public" {
-  for_each = fileset("../client/public", "**/*.*")
+  for_each = fileset("../client/build", "**/*.*")
   bucket = aws_s3_bucket.cf-s3-ecs-demo-bucket.id
   key = each.key
-  source = "../client/public/${each.value}"
-  etag = filemd5("../client/public/${each.value}")
+  source = "../client/build/${each.value}"
+  etag = filemd5("../client/build/${each.value}")
+  content_type = lookup(local.mime_types, regex("\\.[^.]+$", each.value), null)
 }
